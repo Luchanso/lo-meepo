e290e9f520768f29e810026cb197b76e6d3fbe4c
Oleg Loutchansky <luchansodev@gmail.com>
2018-09-24 23:52:29 +0300

    add some positions

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow } = require("electron");
+const electron = require("electron");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
 function createWindow() {
+    const screen = electron.screen.getPrimaryDisplay();
+    const windowSize = {
+        width: 200,
+        height: 200
+    };
     // Create the browser window.
-    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    mainWindow = new BrowserWindow({
+        ...windowSize,
+        x: screen.size.width - windowSize.width - 260,
+        y: screen.size.height - windowSize.height,
+        frame: false,
+        transparent: true
+    });
     mainWindow.setAlwaysOnTop(true);
 
     // and load the index.html of the app.
